65666e29eac38433cede930f6aede7ae90dff6e9
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2021-09-18 18:40:40 +0300

    get it ready for a Windows service

---
+++ ShinyApp.bat
@@ -0,0 +1,6 @@
+@ECHO off
+ 
+
+"C:\Program Files\R\R-4.1.0\bin\x64\Rscript.exe" %~dp0\launch_on_browser.R 
+
+REM >> %~dp0\launch_on_browser.log 2>&1
---
+++ launch_on_browser.R
@@ -0,0 +1,28 @@
+#' This script is used for running the app on the browser via a windows service
+#' 
+#' The Windows task scheduler calls the 'RScript.exe' and the later sources this file
+#' @noRd
+# Set up error log ------------------------------------------------------------
+error_log <- file("C:/Users/pcuser/OneDrive/IMPROVAST/ACS/CSIemail/error_log.Rout", open="wt")
+sink(error_log, type="message")
+
+try({
+  print(Sys.time())
+  
+  library(shiny)
+  folder_address = 'C:/Users/pcuser/OneDrive/IMPROVAST/ACS/CSIemail'
+  
+  x <- system("ipconfig", intern=TRUE)
+  z <- x[grep("IPv4", x)]
+  ip <- gsub(".*? ([[:digit:]])", "\\1", z)
+  print(paste0("the Shiny Web application runs on: http://", ip, ":3845/"))
+  
+  # Run it but don;t launch the browser. Whenever I type the address on  the browser
+  # I will see the app
+  runApp(folder_address, launch.browser=FALSE, port = 3845
+         #, host = getOption("shiny.host", "127.0.0.1")
+         , host = ip
+         
+  )
+  
+})
