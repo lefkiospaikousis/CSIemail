1aace3cf2782902cbd7cfdf1a4da7e609ad9beb2
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2021-11-03 09:57:35 +0200

    global column names

---
+++ data-raw/create_global_values.R
@@ -0,0 +1,32 @@
+## code to prepare `create_global_values` dataset goes here
+
+# Ticket Hour Column names
+col_names_ticket <-
+  
+  # Column label - Variable name
+  c(
+    "Store Code" = "store_code", 
+    "Store name" = "store_name",
+    "Contact" = "contact", 
+    "EMPTY" = "EMPTY", 
+    "Ticket Hour ID" = "ticket_id",
+    "Items" = "items", 
+    "Type" = "type", 
+    "Date" = "date", 
+    "Reference" = "ref",
+    "Details" = "details",
+    "Value" = "value", 
+    "star" = "star", 
+    "os" = "os", 
+    "Debit" = "debit", 
+    "Credit" = "credit"
+  )
+
+
+
+
+usethis::use_data(
+  col_names_ticket
+  
+  , internal = TRUE
+  , overwrite = TRUE)
